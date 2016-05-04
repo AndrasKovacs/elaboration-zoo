7d07877eac4c9cb96e936d83f18b9c3665d01af5
András Kovács <puttamalac@gmail.com>
2016-05-04 08:28:51 +0200

    Remove unneded stuff from Syntax

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # tcbe
-Read [this document](https://github.com/AndrasKovacs/tcbe/blob/master/nosubst.md). 
 
 Currently, the Haskell files are just thrown in without regard to stack project standards, I'll probably do some cleanup later.
--- Syntax.hs
+++ Syntax.hs
@@ -1,22 +1,7 @@
 
-{-# LANGUAGE RecursiveDo, PatternGuards, OverloadedStrings, LambdaCase #-}
-
 module Syntax (RawTerm(..)) where
 
-import Data.Char
 import Data.String
-
-data Token = TStar | TId String | TOpen | TClose | TColon | TDot | TArr
-  deriving (Eq, Show)
-
-instance IsString Token where
-  fromString = \case
-    "*"  -> TStar
-    "->" -> TArr
-    "("  -> TOpen
-    ")"  -> TClose
-    ":"  -> TColon
-    "."  -> TDot
 
 data RawTerm
   = Var !String
